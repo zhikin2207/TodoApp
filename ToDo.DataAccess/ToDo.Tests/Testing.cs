d2a5b52d6e014d0177c85a6f6f27044a543e9c0f
Kotsiuba <vkotsi@softserveinc.com>
2018-11-28 15:17:05 +0200

    Few tests were added.

---
+++ ToDo.DataAccess/ToDo.Tests/Testing.cs
@@ -0,0 +1,105 @@
+﻿using Moq;
+using NUnit.Framework;
+using System;
+using System.Linq;
+using ToDo.DataAccess.Models;
+using ToDo.DataAccess.Repositories.CustomRepositories;
+using ToDo.Services.DTOs;
+using ToDo.Services.Handlers;
+
+namespace ToDo.Tests
+{
+    [TestFixture]
+    public class Testing
+    {
+        public Mock<IItemRepository> mockRepository;
+
+        [OneTimeSetUp]
+        public void TestSetup()
+        {
+            mockRepository = new Mock<IItemRepository>();
+
+            mockRepository
+                .Setup(x => x.GetAll())
+                .Returns(new[]
+                {
+                    new Item {
+                        Title = "Item1",
+                        Description = "xxx",
+                        Priority = Priority.High,
+                        Category = new Category { Name = "Cat1", Parent = null },
+                        DueDate = DateTime.Now, Status = false },
+                    new Item {
+                        Title = "adult",
+                        Description = "Text",
+                        Priority = Priority.Low,
+                        Category = new Category { Name = "Cat2", Parent = null },
+                        DueDate = DateTime.Now, Status = true },
+                    new Item {
+                        Title = "Iasdxxxa",
+                        Description = "Text",
+                        Priority = Priority.Medium,
+                        Category = new Category { Name = "adult", Parent = null },
+                        DueDate = DateTime.Now,
+                        Status = false },
+                    new Item {
+                        Title = "Item4",
+                        Description = "Text",
+                        Priority = Priority.SuperLow,
+                        Category = new Category { Name = "adult", Parent = null },
+                        DueDate = DateTime.Now,
+                        Status = false }
+                 });
+        }
+
+        [Test]
+        public void GetAdultItems_FourDefaultValues_2ElementsWereReturned()
+        { 
+            var elementsCount = new ItemHandler(mockRepository.Object)
+                .GetAdultItems()
+                .Items.Count();
+
+            Assert.AreEqual(2, elementsCount);
+        }
+
+        [Test]
+        public void GetAdultItems_FourDefaultValues_StatusIsFalse()
+        {
+            var itemStatus = new ItemHandler(mockRepository.Object)
+                .GetAdultItems()
+                .Items.FirstOrDefault().Status;
+
+            Assert.AreEqual(false, itemStatus);
+        }
+
+        [Test]
+        public void GetAdultItems_FourDefaultValues_ValuesMatchToRequirements()
+        {
+            var item = new ItemHandler(mockRepository.Object)
+                .GetAdultItems()
+                .Items.FirstOrDefault();
+            Func<ItemDTO,  bool> IsMatchig =(i => (i.Title.Contains("xxx")
+                                || i.Title.Contains("adult")
+                                || string.Equals(
+                                   i.Category.Name,
+                                   "adult",
+                                   StringComparison.CurrentCultureIgnoreCase)
+                                || i.Description.Contains("xxx"))
+                                && !i.Status);
+
+            var match = IsMatchig(item);
+
+            Assert.AreEqual(true, match);
+        }
+
+        [Test]
+        public void GetAdultItems_FourDefaultValues_RightCountOfPriorities()
+        {
+            var countOfHighPriorities = new ItemHandler(mockRepository.Object)
+                .GetAdultItems()
+                .PriorityCounts["High"];
+
+            Assert.AreEqual(1 countOfHighPriorities);
+        }
+    }
+}
